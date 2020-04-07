eed5b66dad7147bb6cc61090afa4957540f95ee3
lstuurman <laurensstuurman678@hotmail.com>
2020-04-07 14:15:25 +0200

    stuff??

--- Code/extract_degree.py
+++ Code/extract_degree.py
@@ -10,8 +10,9 @@ def extract_degrees(path):
     degrees = []
     for f in files:
         print('loading data ',f)
-        pattern = '176N_0.3125V/E '
+        pattern = '176N_0.3125V/E'
         dict = pickle.load(open(f,'rb'))
+        #print(dict.keys())
         name = f.split('_')[-1][:-4]
         for key in dict.keys():
             if pattern in key:
@@ -24,7 +25,7 @@ def extract_degrees(path):
     df = pd.DataFrame(degrees)
     df.columns = ['Degree','iter','type']
     print(df.head())
-    df.to_csv('../results/degrees.csv')
+    df.to_csv('../results/degrees2.csv')
 
 
 extract_degrees('../data/exp1/exp1*.pkl')

057c170e3c453bf0fc133e684e7843e6c97a4164
Laurens Stuurman <laurenssturman678@hotmail.com>
2020-05-10 13:43:51 +0200

    full ln small cells

--- CPM_code/fit_fullLN.py
+++ CPM_code/fit_fullLN.py
@@ -56,7 +56,7 @@ def run_sim(params):
         newtrack = handle_boundaries2(track)
         #cell_tracks[i] = newtrack
         fname = 'LAMBDA_'+str(lambda_act) +'MAX'+str(max_act)+'_' + str(i)
-        np.savetxt('../data/full_LN4/CELL'+fname+'.txt',newtrack)
+        np.savetxt('../data/LN_small_nofrc/CELL'+fname+'.txt',newtrack)
     print('computed : ',params, 'in ',time.time() - t1)
 
     #return data

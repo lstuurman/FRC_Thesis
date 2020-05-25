a88c47c41110aa688b86531cc285a4012fc2caf1
lstuurman <laurensstuurman678@hotmail.com>
2020-05-25 19:13:46 +0200

    stuff

--- CPM_code/seed_density.py
+++ CPM_code/seed_density.py
@@ -17,7 +17,7 @@ def seed_cpm():
     # LAmbdas ; 
     simulation.set_constraints(cell_type = 1,target_area = 150, lambda_area=25)
     simulation.set_constraints(cell_type = 1, lambda_perimeter = .2, target_perimeter = 1500)#8600
-    #simulation.set_constraints(cell_type = 1, lambda_act = 1000,max_act = 10)
+    simulation.set_constraints(cell_type = 1, lambda_act = 2000,max_act = 10)
     # adhesion ; 
     simulation.set_constraints(cell_type = 1,other_cell_type = 1,adhesion = 10)
     simulation.set_constraints(cell_type = 1,other_cell_type = 0,adhesion = 0)
@@ -28,8 +28,8 @@ def seed_cpm():
     rows = []
     size_matrix = []
     # Also write to file : 
-    mat_file = open('size_matrix.txt','w')
-    row_file = open('rows.txt','w')
+    mat_file = open('matrix_moving.txt','w')
+    row_file = open('rows_moving.txt','w')
     s = simulation.get_state()
     # iteratively seed cells : 
     for n in range(int(max_cells)):
@@ -73,13 +73,13 @@ def seed_cpm():
     mat_file.close()
     row_file.close()
     df = pd.DataFrame(data = rows, columns= ['Seeded_cells','Free_voxels','Cell_volume'])
-    df.to_csv('increasing_pressureV150.csv')
+    df.to_csv('increasing_pressureV150_moving.csv')
     # make matrix square :
     length = max(map(len,size_matrix))
     print(size_matrix)
     size_matrix = np.array([xi + [0]*(length - len(xi)) for xi in size_matrix])
     #nparr = np.array(size_matrix)
-    np.savetxt('size_matrix.txt',size_matrix)
+    np.savetxt('size_matrix_moving.txt',size_matrix)
 
 seed_cpm()
 
